0bb2d82e1f4a0859a31bad81eeb6e44272ebf20f
asalhani <asalhani@outlook.com>
2018-04-13 23:29:49 +0300

    working on custom compoent

--- _render/src/app/custom-control/custom-control.component.ts
+++ _render/src/app/custom-control/custom-control.component.ts
@@ -9,34 +9,189 @@ import { FormioComponent, ComponentOptions, ValidateOptions, FormioForm } from '
 })
 export class CustomControlComponent {
 
-  myForm : any
-  
+  myForm: any;
+  customCompForm: any;
+
   constructor() {
-    this.myForm = {
-      title: "Yaqeen control component",
-      group: "layout",
-      template:"tfsdemplate.html",
-      viewTemplate:"tempdsfdslate.html",
+    this.customCompForm = {
       components: [
-          {
-            type: "yaqeenControl",
-            isNew: true,
-            key: "custom",
-            protected: false,
-            persistent: true,
-            placeholder: "Enter your email address",
-            spellcheck:false
+        {
+          type: "yaqeenControl",
+          isNew: true,
+          key: "yaqeenControlComp",
+          spellcheck:true
+        },
+        {
+          autofocus: false,
+          input: true,
+          label: "Submit",
+          tableView: false,
+          ke: "submit",
+          size: "md",
+          leftIcon: "",
+          rightIcon: "",
+          block: false,
+          action: "submit",
+          disableOnInvalid: false,
+          theme: "primary",
+          type: "button"
+        }
+      ],
+      revisions: "",
+      _vid: 0,
+      access: [
+        {
+          roles: [
+            "5acbd1b53c2be2a2ded30239",
+            "5acbd1b53c2be2606cd3023a",
+            "5acbd1b53c2be23d22d3023b"
+          ],
+          type: "read_all"
+        }
+      ],
+      submissionAccess: [],
+      created: "2018-04-13T19:31:53.228Z",
+      _id: "5ad105a939a94bb009f3c045",
+      title: "New form",
+      display: "form",
+      settings: {},
+      name: "newForm",
+      path: "newform",
+      project: "5acbd1b53c2be283ced30238",
+      modified: "2018-04-13T19:31:53.419Z",
+      machineName: "kpeemdlniebvueo:newForm"
+    };
+
+    this.myForm = {
+      "type": "form",
+      "tags": [],
+      "owner": "5acbd180fad38b209c044dc4",
+      "components": [
+        {
+          "autofocus": false,
+          "input": true,
+          "tableView": true,
+          "label": "Identity Type",
+          "key": "identityType",
+          "placeholder": "-- select type --",
+          "data": {
+            "values": [
+              {
+                "value": "nin",
+                "label": "Nin"
+              },
+              {
+                "value": "iqama",
+                "label": "Iqama"
+              }
+            ],
+            "json": "",
+            "url": "",
+            "resource": "",
+            "custom": ""
           },
-          {
-            type: 'textarea',
-            label: 'Content',
-            placeholder: 'Enter content here',
-            wysiwyg: true,
-            key: 'content',
-            input: true,
-            inputType: 'text'
-          }
-      ]
+          "dataSrc": "values",
+          "valueProperty": "",
+          "defaultValue": "",
+          "refreshOn": "",
+          "filter": "",
+          "authenticate": false,
+          "template": "<span>{{ item.label }}</span>",
+          "multiple": false,
+          "protected": false,
+          "unique": false,
+          "persistent": true,
+          "hidden": false,
+          "clearOnHide": true,
+          "validate": {
+            "required": true
+          },
+          "type": "select",
+          "labelPosition": "top",
+          "tags": [],
+          "conditional": {
+            "show": "",
+            "when": null,
+            "eq": ""
+          },
+          "properties": {}
+        },
+        {
+          "autofocus": false,
+          "input": true,
+          "tableView": true,
+          "inputType": "text",
+          "inputMask": "",
+          "label": "Id Number",
+          "key": "idNumber",
+          "placeholder": "ID number",
+          "prefix": "",
+          "suffix": "",
+          "multiple": false,
+          "defaultValue": "",
+          "protected": false,
+          "unique": false,
+          "persistent": true,
+          "hidden": false,
+          "clearOnHide": true,
+          "spellcheck": true,
+          "validate": {
+            "required": true,
+            "minLength": 10,
+            "maxLength": 10,
+            "pattern": "",
+            "custom": "",
+            "customPrivate": false
+          },
+          "conditional": {
+            "show": "",
+            "when": null,
+            "eq": ""
+          },
+          "type": "textfield",
+          "labelPosition": "top",
+          "tags": [],
+          "properties": {}
+        },
+        {
+          "autofocus": false,
+          "input": true,
+          "label": "Submit",
+          "tableView": false,
+          "key": "submit",
+          "size": "md",
+          "leftIcon": "",
+          "rightIcon": "",
+          "block": false,
+          "action": "submit",
+          "disableOnInvalid": false,
+          "theme": "primary",
+          "type": "button"
+        }
+      ],
+      "revisions": "",
+      "_vid": 0,
+      "access": [
+        {
+          "roles": [
+            "5acbd1b53c2be2a2ded30239",
+            "5acbd1b53c2be2606cd3023a",
+            "5acbd1b53c2be23d22d3023b"
+          ],
+          "type": "read_all"
+        }
+      ],
+      "submissionAccess": [],
+      "created": "2018-04-13T19:31:53.228Z",
+      "_id": "5ad105a939a94bb009f3c045",
+      "title": "New form",
+      "display": "form",
+      "settings": {},
+      "name": "newForm",
+      "path": "newform",
+      "project": "5acbd1b53c2be283ced30238",
+      "modified": "2018-04-13T19:31:53.419Z",
+      "machineName": "kpeemdlniebvueo:newForm"
     };
     // this.myForm = {};
     // let validateOptions : ValidateOptions = { };
--- _render/src/app/custom-control/yaqeenControl.ts
+++ _render/src/app/custom-control/yaqeenControl.ts
@@ -6,19 +6,59 @@ import { FormioOptions } from 'angular-formio';
 
 export class YaqeenControlComponent extends BaseComponent {
     [x: string]: any;
-    constructor(component, options, data) {
+    constructor(private component, options, data) {
         super(component, options, data);
     }
     elementInfo() {
-        this.validators.push('email');
-        
-        debugger;
+        // this.validators.push('email');
+
         // tslint:disable-next-line:prefer-const
         let info = super.elementInfo();
         info.type = 'input';
         info.changeEvent = 'input';
-        info.attr.type="email";
-        let test = info.component.validations;
+        info.attr.type = 'text';
+        info.attr.autofocus = true;
+        info.attr.spellcheck =true;
+        // info.attr.inputMask = "";
+         info.attr.label = "Id Number";
+         info.attr.key = "idNumber";
+         info.attr.placeholder = "ID number";
+         info.attr.tableView= true;
+         info.attr.inputType= "text";
+
+        info.attr.multiple= false;
+         info.attr.defaultValue= "dsfsda";
+        // info.attr.protected= false;
+        // info.attr.unique= false;
+        // info.attr.persistent= true;
+         //info.attr.hidden= false;
+        // info.attr.clearOnHide= true;
+        //debugger
+        // info.type = 'input';
+        // info.changeEvent = 'input';
+        // info.attr = {
+
+        //     validate: {
+        //         required: true,
+        //         minLength: 10,
+        //         maxLength: 10,
+        //         pattern: "",
+        //         custom: "",
+        //         customPrivate: false
+        //     },
+        //     conditional: {
+        //         show: "",
+        //         when: null,
+        //         eq: ""
+        //     },
+        //     type: "textfield",
+        //     labelPosition: "top",
+        //     tags: [],
+        //     properties: {}
+
+        // };
+        // info.attr.placeholder = "cccccc";
+        //let test = info.component.validations;
         //info.template = FormioUtils.interpolate("<h1>test</h2>")
         // read proparty from JSON
         // info.attr.spellcheck = this.component.spellcheck;
