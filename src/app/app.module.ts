b2f8b28319565b7ba6801dffafc0e0e3e6f061b2
asalhani <asalhani@outlook.com>
2018-05-08 17:06:08 +0300

    upgrade formio version to alpha-4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,6 @@ import { Formio } from 'formiojs/full';
 import { YaqeenControlComponent } from './custom-control/yaqeenControl';
 import FormioUtils from 'formiojs/utils';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
-import { CustomControlWithTemplateComponent } from './custom-control-with-template/custom-control-with-template.component';
-import { YaqeenControlWithTemplateComponent } from './custom-control-with-template/yaqeenControlWithTemplate';
 import { FormioTeasterComponent } from './formio-teaster/formio-teaster.component';
 import { ServiceLocator } from './services/locator.service';
 import { HttpModule } from '@angular/http';
@@ -27,7 +25,6 @@ import { AppConfig, AuthConfig } from './form-io-app-config';
 
 const appRoutes: Routes = [
   { path: 'form', component: TestFormComponent },
-  { path: 'customwithtemplate', component: CustomControlWithTemplateComponent },
   { path: 'custom', component: CustomControlComponent },
   { path: 'learn', component: LearningResourcesComponent },
   { path: 'fteaster', component: FormioTeasterComponent },
@@ -41,7 +38,6 @@ const appRoutes: Routes = [
     TestFormComponent,
     CustomControlComponent,
     LearningResourcesComponent,
-    CustomControlWithTemplateComponent,
     FormioTeasterComponent,
     FormWithResourcesComponent
   ],
@@ -69,6 +65,5 @@ export class AppModule {
   constructor(private injector: Injector) {
     ServiceLocator.injector = this.injector;
     Formio.registerComponent('yaqeenControl', YaqeenControlComponent);
-    Formio.registerComponent('yaqeenControlWithTemplate', YaqeenControlWithTemplateComponent);
   }
 }
